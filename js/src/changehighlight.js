2b4492086e20a03ce0998a2d66ce14c7c12e73a0
申秋辉 <qiuhui.shen@ele.me>
2019-03-05 02:53:41 +0800

    Site updated: 2019-03-05 02:53:41

--- js/src/changehighlight.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $('.highlight').each(function (idx, val) {
-    var figcaption = $(val);
-    var isTitle = figcaption.find('figcaption').length;
-    if (isTitle) {
-      figcaption.addClass('code-has-title');
-    }
-  });
-});
